115a19752d50a3b8e6e5dc3c4b09fdbcb80c25aa
Leonardo Mendes <leo.89mendes@gmail.com>
2021-06-11 01:58:41 -0300

    setup database

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'amorelimoes' );
+define( 'DB_NAME', 'u311946030_amorelimoes' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'u311946030_ricardo' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '(Ric2021)' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
